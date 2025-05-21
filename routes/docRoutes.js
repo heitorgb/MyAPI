9fa7c3e7942cd83baec095a4cd4224556a169aa0
heitorgb01 <heitor@brsistemas.com.br>
2025-05-21 09:11:26 -0300

    adicionado soma total cred e deb na docController Ajustado os cards no dashboard Adicionado rota para deb e cred

--- routes/docRoutes.js
+++ routes/docRoutes.js
@@ -6,5 +6,7 @@ router.post('/doc', docController.criarDoc);
 router.get('/doc', docController.listarDocs);
 router.delete('/doc/:id', docController.deletarDoc);
 router.delete('/doc', docController.deletarTodos);
+router.get('/doc/totaldeb', docController.totalDocsDeb);
+router.get('/doc/totalcred', docController.totalDocsCred);
 
 module.exports = router;
