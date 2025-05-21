3a6f7761133d615070874b83f1a48255f825bfd6
heitorgb01 <heitor@brsistemas.com.br>
2025-05-21 14:11:52 -0300

    commit de aceito convite contribuinte

--- db/db.js
+++ db/db.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'dbifin',
-    password: 'Ym3.1TkP',
-    port: 5432,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = pool;
+module.exports = pool;
