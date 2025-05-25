eeddc03f297964b991dec78b9eec6b6253ffad2b
Caio Rodrigo <caiorodrigocev@gmail.com>
2025-05-25 20:57:48 -0300

    Implementado Token de sessão

--- routes/docRoutes.js
+++ routes/docRoutes.js
@@ -7,7 +7,5 @@ router.post('/doc',autenticarToken, docController.criarDoc);
 router.get('/doc',autenticarToken, docController.listarDocs);
 router.delete('/doc/:id',autenticarToken, docController.deletarDoc);
 router.delete('/doc',autenticarToken, docController.deletarTodos);
-router.get('/doc/totaldeb',autenticarToken, docController.totalDocsDeb);
-router.get('/doc/totalcred',autenticarToken, docController.totalDocsCred);
 
 module.exports = router;
