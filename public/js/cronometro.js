5fa82be2f850dff4923bfd81172186d20e8b4220
Caio Rodrigo <caiorodrigocev@gmail.com>
2025-05-25 22:21:05 -0300

    time 30m

---
+++ public/js/cronometro.js
@@ -0,0 +1,33 @@
+
+//Rapaziadaaa  W3Schools é o melhor site de programação do mundo, não tem como errar
+
+//W3Schools Example: https://www.w3schools.com/howto/howto_js_countdown.asp
+
+// Set the date we're counting down to
+var countDownDate = new Date().getTime() + 30 * 60 * 1000;
+
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  
+  document.getElementById("cronometro").innerHTML = "Sessão expira em: " + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("cronometro").innerHTML = "EXPIRED";
+  }
+}, 1000);
